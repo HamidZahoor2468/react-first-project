fa6cf2d696232ec692966f12a12a5415efac9b23
Hamid Zahoor <121734693+HamidZahoor2468@users.noreply.github.com>
2024-08-18 00:12:21 -0700

    Second commit

--- src/mod1.mjs
+++
@@ -1,6 +0,0 @@
-import ui, {a, b, c} from './mod2.mjs';
-
-console.log(ui);
-console.log(a);
-console.log(b);
-console.log(c);
--- src/mod2.mjs
+++
@@ -1,9 +0,0 @@
-const a = 34;
-const b = 34;
-const c = 34;
-const d = 34;
-
-export default d;
-export {a};
-export {b};
-export {c};
